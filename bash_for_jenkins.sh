f590f70ad3aa5ab2243fc5484e4b5aaee62d6925
Utsav Bhetwal <utsav_bhetwal2008@yahoo.com>
2019-02-26 18:57:42 -0700

    Update bash_for_jenkins.sh

--- bash_for_jenkins.sh
+++ bash_for_jenkins.sh
@@ -2,3 +2,4 @@
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo &&
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key &&
 sudo yum install jenkins
+# and its done
