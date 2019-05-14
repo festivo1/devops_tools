2628c3f37672025fa557ecbc87f1acfe38341ba9
Utsav Bhetwal <utsav_bhetwal2008@yahoo.com>
2019-05-14 17:11:23 -0500

    java installlation command added

--- bash_for_jenkins.sh
+++ bash_for_jenkins.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #to run jenkins you need jdk so first install java on the machine
+#sudo yum install java-1.8.0-openjdk-devel
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo &&
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key &&
 sudo yum install jenkins
