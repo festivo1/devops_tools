efb235a9e4d5184143162998c14d01e373b5c767
Utsav Bhetwal <utsav_bhetwal2008@yahoo.com>
2019-05-31 00:11:16 -0500

    Update python_for_jenkins.py
    updated

--- python_for_jenkins.py
+++ python_for_jenkins.py
@@ -1,4 +1,4 @@
 import os
 os.system("sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo")
 os.system("sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key")
-os.system("sudo yum install jenkins")
+os.system("sudo yum install jenkins -y")
